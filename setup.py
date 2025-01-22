635b802c6e54cd90af4d43e090fa8f23465749f9
georgeragan <georgeragan123@gmail.com>
2025-01-22 16:07:03 +0530

    second

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="MEDICAL RAG CHATBOT",
+    version="0.0.0",
+    author="Sravan",
+    author_email="sravanlalpp@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
